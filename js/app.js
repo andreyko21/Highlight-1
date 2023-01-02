f3ccca3b14fcad174d6e992347f4f3565278bcc8
andreyko2019 <64082047+andreyko2019@users.noreply.github.com>
2023-01-02 20:44:34 +0200

    v12

--- js/app.js
+++ js/app.js
@@ -3939,6 +3939,51 @@
             filterSelection("all");
         }));
     }
+    "use strict";
+    document.addEventListener("DOMContentLoaded", (function() {
+        const form = document.getElementById("form");
+        form.addEventListener("submit", formSend);
+        async function formSend(e) {
+            e.preventDefault(form);
+            let error = formValidate(form);
+            if (0 === error) {
+                form.reset();
+                modules_flsModules.popup.open("#popup-complite");
+            }
+        }
+        function formValidate(form) {
+            let error = 0;
+            let formReq = document.querySelectorAll("._req");
+            for (let index = 0; index < formReq.length; index++) {
+                const input = formReq[index];
+                formRemoveError(input);
+                if (input.classList.contains("_email")) {
+                    if (emailTest(input)) {
+                        formAddError(input);
+                        error++;
+                    }
+                } else if ("checkbox" === input.getAttribute("type") && false === input.checked) {
+                    formAddError(input);
+                    error++;
+                } else if ("" === input.value) {
+                    formAddError(input);
+                    error++;
+                }
+            }
+            return error;
+        }
+        function formAddError(input) {
+            input.parentElement.classList.add("_error");
+            input.classList.add("_error");
+        }
+        function formRemoveError(input) {
+            input.parentElement.classList.remove("_error");
+            input.classList.remove("_error");
+        }
+        function emailTest(input) {
+            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+        }
+    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
